a73196f07b7264ae2acd7b126243c5fbb6f06ab4
Alaska-999 <kristinagaranchuk@gmail.com>
2020-09-25 14:24:10 +0300

    updated

--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,10 @@
 $(document).ready(function () {
-    $('.featured-products__slider').slick(
-        
-    );
+    $('.featured-products__slider').slick({
+           slidesToShow: 1,
+               slidesToScroll: 1,
+         prevArrow:
+             '<button class="slick-arrow slick-prev"><span class="lnr lnr-chevron-left"></span></button>',
+             nextArrow:
+             '<button class="slick-arrow slick-next"><span class="lnr lnr-chevron-right "></span></button>',
+     });
 });
