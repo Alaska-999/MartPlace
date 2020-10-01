03793cf3e43b339c618071e975642c3c1c4ba68f
Alaska-999 <kristinagaranchuk@gmail.com>
2020-10-01 19:23:02 +0300

    updated

--- app/js/main.js
+++ app/js/main.js
@@ -1,42 +1,41 @@
-jQuery(document).ready(function () {
-    jQuery('.featured-products__slider').slick({
-           slidesToShow: 1,
-               slidesToScroll: 1,
-         prevArrow:
-             '<button class="slider-arrow slick-prev"><span class="lnr lnr-chevron-left"></span></button>',
-             nextArrow:
-             '<button class="slider-arrow slick-next"><span class="lnr lnr-chevron-right "></span></button>',
-     });
-
-
-    jQuery('.follower-feed__catalogue').slick({
-        slidesToShow: 3,
+$(document).ready(function () {
+    $('.featured-products__slider').slick({
+        slidesToShow: 1,
         slidesToScroll: 1,
-         prevArrow:
-             '<button class="catalogue-arrow prev-arrow"><span class="lnr lnr-chevron-left"></span></button>',
-             nextArrow:
-             '<button class="catalogue-arrow next-arrow"><span class="lnr lnr-chevron-right"></span></button>',
+        prevArrow: '<button class="slider-arrow slick-prev"><span class="lnr lnr-chevron-left"></span></button>',
+        nextArrow: '<button class="slider-arrow slick-next"><span class="lnr lnr-chevron-right "></span></button>',
     });
-
-
-
- var mixer = mixitup('.new-release__catalogue');
-
 
 });
+$('.follower-feed__catalogue').slick({
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    prevArrow: '<button class="catalogue-arrow prev-arrow"><span class="lnr lnr-chevron-left"></span></button>',
+    nextArrow: '<button class="catalogue-arrow next-arrow"><span class="lnr lnr-chevron-right"></span></button>',
+});
 
-    jQuery(".rate-stars").rateYo({
-        rating: 5,
-starWidth: '15px',
-normalFill: 'transparent',
-ratedFillBorder: '#ffc000',
- rating: 4.5,
-     halfStar: true
-
-    });
-   
-
-
+$('.testimonials__slider').slick({
+    slidesToShow: 2,
+    slidesToScroll: 1,
+    swipeToSlide: true,
+    //  prevArrow:
+    //      '<button class="testimonials-arrow prev-arrow"><span class="lnr lnr-chevron-left"></span></button>',
+    //      nextArrow:
+    //      '<button class="testimonials-arrow next-arrow"><span class="lnr lnr-chevron-right"></span></button>',
+    //  
+});
 
 
+var mixer = mixitup('.new-release__catalogue');
 
+
+
+//     jQuery(".rate-stars").rateYo({
+//         rating: 5,
+// starWidth: '15px',
+// normalFill: 'transparent',
+// ratedFillBorder: '#ffc000',
+//  rating: 4.5,
+//      halfStar: true
+
+//     });
